ef04667ea721963a2315d433be5a6f84ff513789
Fabian Stolz <stolz@ju029-15.ju029.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2016-09-06 12:50:00 +0200

    made train thread, semaphores and sensors

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,16 +1,57 @@
 import TSim.*;
+import java.util.concurrent.Semaphore;
+import java.lang.InterruptedException;
+import java.util.*;
 
 public class Lab1 {
+    ArrayList<Semaphore> semaphores;
 
   public Lab1(Integer speed1, Integer speed2) {
     TSimInterface tsi = TSimInterface.getInstance();
+    semaphores = new ArrayList<Semaphore>();
+    for(int i = 0; i < 9; i++) {
+       semaphores.add(new Semaphore(1));
+    }
 
     try {
-      tsi.setSpeed(1,speed1);
+      TrainThread train1 = new TrainThread(1, speed1, semaphores);
+      TrainThread train2 = new TrainThread(2, speed2, semaphores);
+
+      train1.start();
+      train2.start();
+      train1.join();
+      train2.join();
     }
-    catch (CommandException e) {
-      e.printStackTrace();    // or only e.getMessage() for the error
+    catch (InterruptedException e) {
+      e.printStackTrace();
       System.exit(1);
     }
   }
+}
+
+class TrainThread extends Thread {
+   private int speed;
+   private int trainNumber;
+   private ArrayList<Semaphore> semaphores;
+   private TSimInterface tsi;
+
+   TrainThread(int trainNumber, int speed, ArrayList<Semaphore> semaphores) {
+      this.trainNumber = trainNumber;
+      this.speed = speed;
+      this.semaphores = semaphores;
+      this.tsi = TSimInterface.getInstance();
+
+      System.out.println("hello from train " + trainNumber);
+   }
+
+   public void run() {
+    try {
+
+      tsi.setSpeed(trainNumber, speed);
+    }
+    catch (CommandException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+   }
 }
